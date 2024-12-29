af49654f25a3918e0fd9bf3f642021ab4c2b95cd
Nirmal Tamang <lamanirmal27@gmail.com>
2024-12-29 21:16:53 +0545

    fixes#1

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,6 +2,7 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.model.js";
 
+
 const configureGoogleOAuthStrategy = () => {
   passport.use(
     new GoogleStrategy(
--- backend/middleware/sessionConfig.js
+++ backend/middleware/sessionConfig.js
@@ -1,25 +1,21 @@
+import MongoStore from "connect-mongo";
 import session from "express-session";
-import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 const sessionConfig = session({
+
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    secure: process.env.NODE_ENV === "production", // Only use secure in production
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    httpOnly: true,
-  },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    ttl: 14 * 24 * 60 * 60
+  }),
+  // cookie: {
+  //   secure: process.env.NODE_ENV === "production", // Only use secure in production
+  //   maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  //   httpOnly: true,
+  // },
 });
-
-// export const initializeSession = (req, res, next) => {
-
-
-//   if (req.user) {
-//     generateTokenAndSetCookie(req.user._id, res);
-//   }
-//   next();
-// };
 
 export default sessionConfig;
