6fe1a232154482a9b24153298ab9db35960fa20d
Nirmal Tamang <lamanirmal27@gmail.com>
2025-02-01 06:33:21 +0545

    google auth#1

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,11 +7,13 @@ const generateTokenAndSetCookie = (userId, res) => {
     expiresIn: "15d",
   });
 
+  const isProduction = process.env.DEV_ENV === "production";
+
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "None",
-    secure: true,
+    secure: isProduction,
   });
 };
 
